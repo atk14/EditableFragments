0953f26cb0ada6fc51642d2317bd0d9a07a5ff41
Jaromir Tomek <jaromir.tomek@gmail.com>
2021-10-13 15:18:42 +0200

    Added method EditableFragmentHistory::getCreatedByUser()

--- src/app/models/editable_fragment_history.php
+++ src/app/models/editable_fragment_history.php
@@ -4,4 +4,8 @@ class EditableFragmentHistory extends ApplicationModel{
 	function __construct(){
 		parent::__construct("editable_fragment_history");
 	}
+
+	function getCreatedByUser(){
+		return Cache::Get("User",$this->getCreatedByUserId());
+	}
 }
